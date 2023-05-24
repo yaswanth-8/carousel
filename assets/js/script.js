8fedb70cca2976a415220d7fa293d5171ab7f2f4
yaswanth <ya8wanth@gmail.com>
2023-05-24 12:43:05 +0530

    ADD.implemented carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,28 @@ slides.forEach((slide) => {
   slide.style.backgroundColor = 'white';
 });
 
+prevBtn.addEventListener('click', showPreviousSlide);
+nextBtn.addEventListener('click', showNextSlide);
+
+
 colorPickers.forEach((picker, index) => {
   picker.addEventListener('change', () => {
     slides[index].style.backgroundColor = picker.value;
   });
 });
+
+function showPreviousSlide() {
+  slides[currentIndex].style.transform = 'translateX(100%)';
+  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+  setTimeout(() => {
+    slides[currentIndex].style.transform = 'translateX(0)';
+  }, 0);
+}
+
+function showNextSlide() {
+  slides[currentIndex].style.transform = 'translateX(-100%)';
+  currentIndex = (currentIndex + 1) % slides.length;
+  setTimeout(() => {
+    slides[currentIndex].style.transform = 'translateX(0)';
+  }, 0);
+}
