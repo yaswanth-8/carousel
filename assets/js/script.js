a73bf73d8063a6cddb7345048ea53e31e8e7072e
yaswanth <ya8wanth@gmail.com>
2023-05-25 15:32:15 +0530

    UPDATE. SLide no. and hex value are displayed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 const slides = document.querySelectorAll('.slide');
+const heading = document.querySelectorAll('h1');
 const colorPickers = document.querySelectorAll('.color-picker');
 const prevBtn = document.querySelector('.prev');
 const nextBtn = document.querySelector('.next');
@@ -15,6 +16,12 @@ nextBtn.addEventListener('click', showNextSlide);
 colorPickers.forEach((picker, index) => {
   picker.addEventListener('change', () => {
     slides[index].style.backgroundColor = picker.value;
+    if((index+2)%5 == 0){
+      heading[index].innerText =`slide no (5)  |  hex value - ${picker.value}` ;
+    }
+    else{
+    heading[index].innerText =`slide no (${(index+2)%5})  |  hex value - ${picker.value}` ;
+    }
   });
 });
 
